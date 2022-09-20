e97f4c2aaf7ce7f0e98ec325f1286150b76a4e28
Pyerdowah <101801820+Pyerdowah@users.noreply.github.com>
2022-09-20 15:23:48 +0200

    tests improvement v2

--- src/test/java/com/example/currency/service/CurrencyServiceTest.java
+++ src/test/java/com/example/currency/service/CurrencyServiceTest.java
@@ -16,19 +16,8 @@ public class CurrencyServiceTest {
     @Test
     public void split_data() {
         //given
-        String[] startDateArray = new String[5];
-        startDateArray[0] = "2022-09-10";
-        startDateArray[1] = "2022-09-10";
-        startDateArray[2] = "2002-01-02";
-        startDateArray[3] = "2022-09-18";
-        startDateArray[4] = "2000-01-01";
-
-        String[] endDateArray = new String[5];
-        endDateArray[0] = "2022-09-11";
-        endDateArray[1] = "2022-09-11";
-        endDateArray[2] = "2002-01-03";
-        endDateArray[3] = "2022-09-11";
-        endDateArray[4] = "2002-01-03";
+        String[] startDateArray = {"2022-09-10", "2022-09-10", "2002-01-02", "2022-09-18", "2000-01-01"};
+        String[] endDateArray = {"2022-09-11", "2022-09-11", "2002-01-03", "2022-09-11", "2002-01-03"};
 
         CurrencyResponseDto currencyResponseDto = new CurrencyResponseDto(3.9451, 4.0249, "2002-01-02", -0.0798);
         CurrencyResponseDto currencyResponseDto1 = new CurrencyResponseDto(3.8994, 3.9782, "2002-01-03", -0.0788);
@@ -36,36 +25,17 @@ public class CurrencyServiceTest {
         currencyResponseDtoList.add(currencyResponseDto);
         currencyResponseDtoList.add(currencyResponseDto1);
 
+        HttpStatus[] httpStatusCodeTest = {HttpStatus.NOT_FOUND, HttpStatus.NOT_FOUND, HttpStatus.OK, HttpStatus.BAD_REQUEST, HttpStatus.OK};
+        String[] currencyResponseBodyTest = {"no data in this time", "no data in this time", currencyResponseDtoList.toString(), "wrong data range", currencyResponseDtoList.toString()};
+
         CurrencyService currencyService = new CurrencyService();
 
-        //when
-        ResponseEntity<?> responseEntity = currencyService.splitData(startDateArray[0], endDateArray[0]);
-        //then
-        assertEquals(responseEntity.getStatusCode(), HttpStatus.NOT_FOUND);
-        assertEquals(responseEntity.getBody(), "no data in this time");
-
-        //when
-        ResponseEntity<?> responseEntity1 = currencyService.splitData(startDateArray[1], endDateArray[1]);
-        //then
-        assertEquals(responseEntity1.getStatusCode(), HttpStatus.NOT_FOUND);
-        assertEquals(responseEntity1.getBody(), "no data in this time");
-
-        //when
-        ResponseEntity<?> responseEntity2 = currencyService.splitData(startDateArray[2], endDateArray[2]);
-        //then
-        assertEquals(responseEntity2.getStatusCode(), HttpStatus.OK);
-        assertEquals(Objects.requireNonNull(responseEntity2.getBody()).toString(), currencyResponseDtoList.toString());
-
-        //when
-        ResponseEntity<?> responseEntity3 = currencyService.splitData(startDateArray[3], endDateArray[3]);
-        //then
-        assertEquals(responseEntity3.getStatusCode(), HttpStatus.BAD_REQUEST);
-        assertEquals(responseEntity3.getBody(), "wrong data range");
-
-        //when
-        ResponseEntity<?> responseEntity4 = currencyService.splitData(startDateArray[4], endDateArray[4]);
-        //then
-        assertEquals(responseEntity4.getStatusCode(), HttpStatus.OK);
-        assertEquals(Objects.requireNonNull(responseEntity4.getBody()).toString(), currencyResponseDtoList.toString());
+        for (int i = 0; i < 5; i++) {
+            //when
+            ResponseEntity<?> responseEntity = currencyService.splitData(startDateArray[i], endDateArray[i]);
+            //then
+            assertEquals(responseEntity.getStatusCode(), httpStatusCodeTest[i]);
+            assertEquals(Objects.requireNonNull(responseEntity.getBody()).toString(), currencyResponseBodyTest[i]);
+        }
     }
 }
--- src/test/java/com/example/currency/service/DateServiceTest.java
+++ src/test/java/com/example/currency/service/DateServiceTest.java
@@ -13,84 +13,45 @@ public class DateServiceTest {
     @Test
     public void date_service() {
         //given
-        String[] startDateArray = new String[6];
-        startDateArray[0] = "2022-09-11"; //false
-        startDateArray[1] = "2000-09-11"; //false
-        startDateArray[2] = "2023-09-11"; //true
-        startDateArray[3] = "2023-09-11"; //true
-        startDateArray[4] = "2023-09-11"; //true
-        startDateArray[5] = "2000-09-11"; //false
+        String[] startDateArray = {"2022-09-11", "2000-09-11", "2023-09-11", "2023-09-11", "2023-09-11", "2000-09-11"};
+        String[] endDateArray = {"2022-09-18", "2022-09-16", "2000-09-16", "2022-09-17", "2023-09-15", "2000-10-11"};
+        LocalDate[] startDateTest = {LocalDate.parse(startDateArray[0]), Constants.availableDataFirstDay, LocalDate.parse(startDateArray[2]), LocalDate.parse(startDateArray[3]), LocalDate.parse(startDateArray[4]), Constants.availableDataFirstDay};
+        LocalDate[] endDateTest = {LocalDate.parse(endDateArray[0]), LocalDate.parse(endDateArray[1]), LocalDate.parse(endDateArray[2]), LocalDate.parse(endDateArray[3]), LocalDate.now(), LocalDate.parse(endDateArray[5])};
+        Boolean[] dataAvailableTest = {false, false, false, false, true, true};
+        Long[] daysBetweenTest = {
+                DAYS.between(LocalDate.parse(startDateArray[0]), LocalDate.parse(endDateArray[0])),
+                DAYS.between(Constants.availableDataFirstDay, LocalDate.parse(endDateArray[1])),
+                DAYS.between(LocalDate.parse(startDateArray[2]), LocalDate.parse(endDateArray[2])),
+                DAYS.between(LocalDate.parse(startDateArray[3]), LocalDate.parse(endDateArray[3])),
+                DAYS.between(LocalDate.parse(startDateArray[4]), LocalDate.parse(LocalDate.now().toString())),
+                DAYS.between(Constants.availableDataFirstDay, LocalDate.parse(endDateArray[5]))};
+        Long[] counterTest = {
+                DAYS.between(LocalDate.parse(startDateArray[0]), LocalDate.parse(endDateArray[0])) / Constants.maxDaysDataRange,
+                DAYS.between(Constants.availableDataFirstDay, LocalDate.parse(endDateArray[1])) / Constants.maxDaysDataRange,
+                DAYS.between(LocalDate.parse(startDateArray[2]), LocalDate.parse(endDateArray[2])) / Constants.maxDaysDataRange,
+                DAYS.between(LocalDate.parse(startDateArray[3]), LocalDate.parse(endDateArray[3])) / Constants.maxDaysDataRange,
+                DAYS.between(LocalDate.parse(startDateArray[4]), LocalDate.parse(LocalDate.now().toString())) / Constants.maxDaysDataRange,
+                DAYS.between(Constants.availableDataFirstDay, LocalDate.parse(endDateArray[5])) / Constants.maxDaysDataRange};
+        Long[] restTest = {
+                DAYS.between(LocalDate.parse(startDateArray[0]), LocalDate.parse(endDateArray[0])) % Constants.maxDaysDataRange,
+                DAYS.between(Constants.availableDataFirstDay, LocalDate.parse(endDateArray[1])) % Constants.maxDaysDataRange,
+                DAYS.between(LocalDate.parse(startDateArray[2]), LocalDate.parse(endDateArray[2])) % Constants.maxDaysDataRange,
+                DAYS.between(LocalDate.parse(startDateArray[3]), LocalDate.parse(endDateArray[3])) % Constants.maxDaysDataRange,
+                DAYS.between(LocalDate.parse(startDateArray[4]), LocalDate.parse(LocalDate.now().toString())) % Constants.maxDaysDataRange,
+                DAYS.between(Constants.availableDataFirstDay, LocalDate.parse(endDateArray[5])) % Constants.maxDaysDataRange};
 
-        String[] endDateArray = new String[6];
-        endDateArray[0] = "2022-09-18"; //false //false
-        endDateArray[1] = "2022-09-16"; //false //false
-        endDateArray[2] = "2000-09-16"; //true //false
-        endDateArray[3] = "2022-09-17"; //false //false
-        endDateArray[4] = "2023-09-15"; //false //true
-        endDateArray[5] = "2000-10-11"; //true // true
-
-        //when
-        DateService dateService = new DateService(startDateArray[0], endDateArray[0]);
-        DateService dateService1 = new DateService(startDateArray[1], endDateArray[1]);
-        DateService dateService2 = new DateService(startDateArray[2], endDateArray[2]);
-        DateService dateService3 = new DateService(startDateArray[3], endDateArray[3]);
-        DateService dateService4 = new DateService(startDateArray[4], endDateArray[4]);
-        DateService dateService5 = new DateService(startDateArray[5], endDateArray[5]);
-
-        //then
-        assertEquals(dateService.getLocalStartDate(), LocalDate.parse(startDateArray[0]));
-        assertEquals(dateService.getLocalEndDate(), LocalDate.parse(endDateArray[0]));
-        assertEquals(dateService.getToday(), LocalDate.now());
-        assertFalse(dateService.getIsDataAvailable());
-        assertEquals(dateService.getDaysBetweenStartAndEnd(), DAYS.between(LocalDate.parse(startDateArray[0]), LocalDate.parse(endDateArray[0])));
-        assertEquals(dateService.getCounter(), DAYS.between(LocalDate.parse(startDateArray[0]), LocalDate.parse(endDateArray[0])) / Constants.maxDaysDataRange);
-        assertEquals(dateService.getRest(), DAYS.between(LocalDate.parse(startDateArray[0]), LocalDate.parse(endDateArray[0])) % Constants.maxDaysDataRange);
-
-        assertEquals(dateService1.getLocalStartDate(), Constants.availableDataFirstDay);
-        assertEquals(dateService1.getLocalEndDate(), LocalDate.parse(endDateArray[1]));
-        assertEquals(dateService1.getToday(), LocalDate.now());
-        assertFalse(dateService1.getIsDataAvailable());
-        assertEquals(dateService1.getDaysBetweenStartAndEnd(), DAYS.between(Constants.availableDataFirstDay, LocalDate.parse(endDateArray[1])));
-        assertEquals(dateService1.getCounter(), DAYS.between(Constants.availableDataFirstDay, LocalDate.parse(endDateArray[1])) / Constants.maxDaysDataRange);
-        assertEquals(dateService1.getRest(), DAYS.between(Constants.availableDataFirstDay, LocalDate.parse(endDateArray[1])) % Constants.maxDaysDataRange);
-
-
-        assertEquals(dateService2.getLocalStartDate(), LocalDate.parse(startDateArray[2]));
-        assertEquals(dateService2.getLocalEndDate(), LocalDate.parse(endDateArray[2]));
-        assertEquals(dateService2.getToday(), LocalDate.now());
-        assertFalse(dateService2.getIsDataAvailable());
-        assertEquals(dateService2.getDaysBetweenStartAndEnd(), DAYS.between(LocalDate.parse(startDateArray[2]), LocalDate.parse(endDateArray[2])));
-        assertEquals(dateService2.getCounter(), DAYS.between(LocalDate.parse(startDateArray[2]), LocalDate.parse(endDateArray[2])) / Constants.maxDaysDataRange);
-        assertEquals(dateService2.getRest(), DAYS.between(LocalDate.parse(startDateArray[2]), LocalDate.parse(endDateArray[2])) % Constants.maxDaysDataRange);
-
-
-        assertEquals(dateService3.getLocalStartDate(), LocalDate.parse(startDateArray[3]));
-        assertEquals(dateService3.getLocalEndDate(), LocalDate.parse(endDateArray[3]));
-        assertEquals(dateService3.getToday(), LocalDate.now());
-        assertFalse(dateService3.getIsDataAvailable());
-        assertEquals(dateService3.getDaysBetweenStartAndEnd(), DAYS.between(LocalDate.parse(startDateArray[3]), LocalDate.parse(endDateArray[3])));
-        assertEquals(dateService3.getCounter(), DAYS.between(LocalDate.parse(startDateArray[3]), LocalDate.parse(endDateArray[3])) / Constants.maxDaysDataRange);
-        assertEquals(dateService3.getRest(), DAYS.between(LocalDate.parse(startDateArray[3]), LocalDate.parse(endDateArray[3])) % Constants.maxDaysDataRange);
-
-
-        assertEquals(dateService4.getLocalStartDate(), LocalDate.parse(startDateArray[4]));
-        assertEquals(dateService4.getLocalEndDate(), dateService4.getToday());
-        assertEquals(dateService4.getToday(), LocalDate.now());
-        assertTrue(dateService4.getIsDataAvailable());
-        assertEquals(dateService4.getDaysBetweenStartAndEnd(), DAYS.between(LocalDate.parse(startDateArray[4]), LocalDate.parse(LocalDate.now().toString())));
-        assertEquals(dateService4.getCounter(), DAYS.between(LocalDate.parse(startDateArray[4]), LocalDate.parse(LocalDate.now().toString())) / Constants.maxDaysDataRange);
-        assertEquals(dateService4.getRest(), DAYS.between(LocalDate.parse(startDateArray[4]), LocalDate.parse(LocalDate.now().toString())) % Constants.maxDaysDataRange);
-
-
-        assertEquals(dateService5.getLocalStartDate(), Constants.availableDataFirstDay);
-        assertEquals(dateService5.getLocalEndDate(), LocalDate.parse(endDateArray[5]));
-        assertEquals(dateService5.getToday(), LocalDate.now());
-        assertTrue(dateService5.getIsDataAvailable());
-        assertEquals(dateService5.getDaysBetweenStartAndEnd(), DAYS.between(Constants.availableDataFirstDay, LocalDate.parse(endDateArray[5])));
-        assertEquals(dateService5.getCounter(), DAYS.between(Constants.availableDataFirstDay, LocalDate.parse(endDateArray[5])) / Constants.maxDaysDataRange);
-        assertEquals(dateService5.getRest(), DAYS.between(Constants.availableDataFirstDay, LocalDate.parse(endDateArray[5])) % Constants.maxDaysDataRange);
-
-
+        for (int i = 0; i < 5; i++) {
+            //when
+            DateService dateService = new DateService(startDateArray[i], endDateArray[i]);
+            //then
+            assertEquals(dateService.getLocalStartDate(), startDateTest[i]);
+            assertEquals(dateService.getLocalEndDate(), endDateTest[i]);
+            assertEquals(dateService.getToday(), LocalDate.now());
+            assertEquals(dateService.getIsDataAvailable(), dataAvailableTest[i]);
+            assertEquals(dateService.getDaysBetweenStartAndEnd(), daysBetweenTest[i]);
+            assertEquals(dateService.getCounter(), counterTest[i]);
+            assertEquals(dateService.getRest(), restTest[i]);
+        }
     }
 
     @Test
